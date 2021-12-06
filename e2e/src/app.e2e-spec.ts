5ffbc07fc3f54d5b856f0fdd66a4325fe25a00f7
Kunwoo <ykw0628@gmail.com>
2021-12-06 15:43:25 -0500

    created components

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('angular-recipr app is running!');
+    expect(await page.getTitleText()).toEqual('angular-recipe app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-recipr'`, () => {
+  it(`should have as title 'angular-recipe'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-recipr');
+    expect(app.title).toEqual('angular-recipe');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-recipr app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-recipe app is running!');
   });
 });
