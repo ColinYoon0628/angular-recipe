be4395975aaab1651994847b9d061a7427cc4131
Kunwoo <ykw0628@gmail.com>
2021-12-06 16:31:13 -0500

    updated navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,5 +5,5 @@ import { Component } from "@angular/core";
     templateUrl:'./header.component.html'
 })
 export class HeaderComponent {
-    title: string = 'Recipe';
+    collapsed: boolean = true;
 }
